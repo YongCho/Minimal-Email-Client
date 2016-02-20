d2b4c400e1a92d236beecc7feb1196fcc3b9bda5
YongCho <yongjec@gmail.com>
2016-02-20 15:34:07 -0500

    Rename directory and assemblies to reflect new project name. Add icons.

--- MinimalEmailClient/Properties/AssemblyInfo.cs
+++ MinimalEmailClient/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EmailClientPrototype2")]
+[assembly: AssemblyTitle("MinimalEmailClient")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EmailClientPrototype2")]
+[assembly: AssemblyProduct("MinimalEmailClient")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
