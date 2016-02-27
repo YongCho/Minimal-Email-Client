68f70300e68549845e81a6cdd09cb42b2cc7da36
YongCho <yongjec@gmail.com>
2016-02-27 12:24:25 -0500

    Move Message list to a separate view.

--- MinimalEmailClient/ViewModels/MainWindowViewModel.cs
+++ MinimalEmailClient/ViewModels/MainWindowViewModel.cs
@@ -1,60 +1,24 @@
 ﻿using MinimalEmailClient.Models;
-using MinimalEmailClient.Events;
-using System.Collections.ObjectModel;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Windows.Input;
-using Prism.Mvvm;
 using Prism.Commands;
 using Prism.Interactivity.InteractionRequest;
-using Prism.Events;
+using Prism.Mvvm;
+using System.Windows.Input;
 
 namespace MinimalEmailClient.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        public ObservableCollection<Message> Messages { get; set; }
-        public Message SelectedMessage { get; set; }
         public InteractionRequest<WriteNewMessageNotification> WriteNewMessagePopupRequest { get; set; }
         public InteractionRequest<INotification> AddNewAccountPopupRequest { get; set; }
         public ICommand WriteNewMessageCommand { get; set; }
         public ICommand AddNewAccountCommand { get; set; }
-        private IEventAggregator eventAggregator;
 
         public MainWindowViewModel()
         {
-            Messages = new ObservableCollection<Message>();
             WriteNewMessagePopupRequest = new InteractionRequest<WriteNewMessageNotification>();
             AddNewAccountPopupRequest = new InteractionRequest<INotification>();
             WriteNewMessageCommand = new DelegateCommand(RaiseWriteNewMessagePopupRequest);
             AddNewAccountCommand = new DelegateCommand(RaiseAddNewAccountPopupRequest);
-            this.eventAggregator = GlobalEventAggregator.Instance().EventAggregator;
-            this.eventAggregator.GetEvent<MailboxSelectionEvent>().Subscribe(HandleMailboxSelection);
-        }
-
-        private void HandleMailboxSelection(Mailbox selectedMailbox)
-        {
-            Debug.WriteLine("Selected Mailbox:\n" + selectedMailbox.ToString());
-        }
-
-        public async void Sync(Account account)
-        {
-            List<Message> msgs = await Task.Run<List<Message>>(() =>
-            {
-                Downloader downloader = new Downloader(account);
-                return downloader.GetDummyMessages();
-            });
-
-            foreach (Message m in msgs)
-            {
-                Messages.Add(m);
-            }
-
-            foreach (Message m in Messages)
-            {
-                Debug.WriteLine(m.ToString());
-            }
         }
 
         private void RaiseWriteNewMessagePopupRequest()
---
+++ MinimalEmailClient/ViewModels/MessageListViewModel.cs
@@ -0,0 +1,50 @@
+﻿using MinimalEmailClient.Events;
+using MinimalEmailClient.Models;
+using Prism.Events;
+using Prism.Mvvm;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace MinimalEmailClient.ViewModels
+{
+    public class MessageListViewModel : BindableBase
+    {
+        public ObservableCollection<Message> Messages { get; set; }
+        public Message SelectedMessage { get; set; }
+        private IEventAggregator eventAggregator;
+
+        public MessageListViewModel()
+        {
+            Messages = new ObservableCollection<Message>();
+            this.eventAggregator = GlobalEventAggregator.Instance().EventAggregator;
+            this.eventAggregator.GetEvent<MailboxSelectionEvent>().Subscribe(HandleMailboxSelection);
+        }
+
+        private void HandleMailboxSelection(Mailbox selectedMailbox)
+        {
+            Debug.WriteLine("Selected Mailbox:\n" + selectedMailbox.ToString());
+        }
+
+        public async void Sync(Account account)
+        {
+            List<Message> msgs = await Task.Run<List<Message>>(() =>
+            {
+                Downloader downloader = new Downloader(account);
+                return downloader.GetDummyMessages();
+            });
+
+            foreach (Message m in msgs)
+            {
+                Messages.Add(m);
+            }
+
+            foreach (Message m in Messages)
+            {
+                Debug.WriteLine(m.ToString());
+            }
+        }
+
+    }
+}
