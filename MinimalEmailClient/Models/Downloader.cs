88388aa0850e1e72153afc34ccae521d781d2214
YongCho <yongjec@gmail.com>
2016-02-28 07:03:05 -0500

    Parse fetched response string in a separate class.
    Parsing is becoming an arduous task. Let's move that functionality out of
    Downloader.

--- MinimalEmailClient/Models/Downloader.cs
+++ MinimalEmailClient/Models/Downloader.cs
@@ -184,7 +184,7 @@ namespace MinimalEmailClient.Models
                         string untaggedResponse = match.Groups[2].ToString();
                         Debug.WriteLine(untaggedResponse);
 
-                        Message message = ParseFetchResponse(untaggedResponse);
+                        Message message = FetchParser.CreateHeader(untaggedResponse);
 
                         if (message != null)
                         {
@@ -224,48 +224,6 @@ namespace MinimalEmailClient.Models
             }
 
             return messages;
-        }
-
-        // Constructs a Message object from a response string returned by a FETCH command.
-        Message ParseFetchResponse(string untaggedResponse)
-        {
-            Message message = new Message();
-
-            string itemHeaderPattern = "^(.*)\r\n";
-            string itemHeader = Regex.Match(untaggedResponse, itemHeaderPattern).Groups[1].ToString();
-            Debug.WriteLine("Item Header: " + itemHeader);
-
-            string subjectPattern = "\r\nSubject: (.*)\r\n";
-            string subject = Regex.Match(untaggedResponse, subjectPattern).Groups[1].ToString();
-            subject = Decoder.DecodeHeaderElement(subject);
-            Debug.WriteLine("Subject: " + subject);
-            message.Subject = subject;
-
-            string datePattern = "\r\nDate: (.*)\r\n";
-            string dtString = Regex.Match(untaggedResponse, datePattern, RegexOptions.IgnoreCase).Groups[1].ToString();
-            Debug.WriteLine("Date: " + dtString);
-            DateTime dt;
-            if (!DateTime.TryParse(dtString, out dt))
-            {
-                dt = DateTimeParser.Parse(dtString);
-            }
-            message.DateString = dtString;
-            message.Date = dt;
-
-            string senderPattern = "\r\nFrom: (.*)<(.*)>\r\n";
-            Match m = Regex.Match(untaggedResponse, senderPattern);
-            string senderName = Decoder.DecodeHeaderElement(m.Groups[1].ToString());
-            string senderAddress = m.Groups[2].ToString();
-            if (senderName.Trim() == string.Empty)
-            {
-                senderName = senderAddress;
-            }
-            Debug.WriteLine("Sender Name: " + senderName);
-            Debug.WriteLine("Sender Address: " + senderAddress);
-            message.SenderAddress = senderAddress;
-            message.SenderName = senderName;
-
-            return message;
         }
 
         private void Logout()
