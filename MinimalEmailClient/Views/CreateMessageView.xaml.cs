e14de05426589b37ec2ee8b3580d47bbc1aab2b4
umin1234 <yoo_cha@student.uml.edu>
2016-03-13 00:36:18 -0500

    Border, autofill From textbox

--- MinimalEmailClient/Views/CreateMessageView.xaml.cs
+++ MinimalEmailClient/Views/CreateMessageView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows.Controls;
 using MinimalEmailClient.ViewModels;
+using MinimalEmailClient.Models;
+using System.Diagnostics;
 
 namespace MinimalEmailClient.Views
 {
@@ -8,10 +10,38 @@ namespace MinimalEmailClient.Views
     /// </summary>
     public partial class CreateMessageView : UserControl
     {
+        private string smtpServerName;
+        private int smtpPortNumber;
+        private string smtpLoginName;
+        private string smtpLoginPassword;
+        
         public CreateMessageView()
         {
             this.DataContext = new CreateMessageViewModel();
             InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            AccountManager accountManager = AccountManager.Instance();
+            if (accountManager.Accounts.Count > 0)
+            {
+                Account account = accountManager.Accounts[0];
+                this.From.Text = account.EmailAddress;
+                this.smtpServerName = account.SmtpServerName;
+                this.smtpPortNumber = account.SmtpPortNumber;
+                this.smtpLoginName = account.SmtpLoginName;
+                this.smtpLoginPassword = account.SmtpLoginPassword;
+
+                Trace.WriteLine("Smtp Server Name: " + this.smtpServerName);
+                Trace.WriteLine("Smtp Port Number: " + this.smtpPortNumber);
+                Trace.WriteLine("Smtp Login Name: " + this.smtpLoginName);
+            }
+        }
+
+        private void SendButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Trace.WriteLine("Sending...");
         }
     }
 }
