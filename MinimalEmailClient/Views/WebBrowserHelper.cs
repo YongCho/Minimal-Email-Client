94b25e5e03f7654b394476a9b7e2c86ebf43b428
YongCho <yongjec@gmail.com>
2016-03-24 19:25:40 -0400

    Add a helper class for binding html string to WebBrowser control
    although we are not using the WebBrowser control yet, I have tested this
    class in a separate project.

---
+++ MinimalEmailClient/Views/WebBrowserHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MinimalEmailClient.Views
+{
+    // This class is used to create a binding property to a WebBrowser control.
+    // Source: http://stackoverflow.com/questions/4202961/can-i-bind-html-to-a-wpf-web-browser-control
+    // XAML example:
+    // <WebBrowser local:WebBrowserHelper.Html="{Binding MyHtmlString}" />
+    public class WebBrowserHelper
+    {
+        public static readonly DependencyProperty HtmlProperty = DependencyProperty.RegisterAttached(
+            "Html",
+            typeof(string),
+            typeof(WebBrowserHelper),
+            new FrameworkPropertyMetadata(OnHtmlChanged));
+
+        [AttachedPropertyBrowsableForType(typeof(WebBrowser))]
+        public static string GetHtml(WebBrowser d)
+        {
+            return (string)d.GetValue(HtmlProperty);
+        }
+
+        public static void SetHtml(WebBrowser d, string value)
+        {
+            d.SetValue(HtmlProperty, value);
+        }
+
+        static void OnHtmlChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            WebBrowser webBrowser = dependencyObject as WebBrowser;
+            if (webBrowser != null)
+                webBrowser.NavigateToString(e.NewValue as string ?? "&nbsp;");
+        }
+    }
+}
