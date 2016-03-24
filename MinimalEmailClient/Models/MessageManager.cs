d8dbc40b14e08ce7c62654377744984db5526b36
YongCho <yongjec@gmail.com>
2016-03-24 12:08:25 -0400

    Move message sync methods to a separate class.
    MessageListViewModel was getting cluttered so I created a new class to
    deal with all message related methods.

---
+++ MinimalEmailClient/Models/MessageManager.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace MinimalEmailClient.Models
+{
+    public class MessageManager
+    {
+        public List<Message> Messages;
+        public string Error = string.Empty;
+        public event EventHandler<Message> MessageAdded;
+
+        private static MessageManager instance;
+        protected MessageManager()
+        {
+            LoadMessagesFromDb();
+        }
+        public static MessageManager Instance()
+        {
+            if (instance == null)
+            {
+                instance = new MessageManager();
+            }
+            return instance;
+        }
+
+        protected virtual void OnMessageAdded(Message newMessage)
+        {
+            if (MessageAdded != null)
+            {
+                MessageAdded(this, newMessage);
+            }
+        }
+
+        private void LoadMessagesFromDb()
+        {
+            if (Messages == null)
+            {
+                Messages = new List<Message>();
+            }
+            else
+            {
+                Messages.Clear();
+            }
+
+            Messages = DatabaseManager.GetMessages();
+        }
+
+        public void BeginSyncMessages(Account account)
+        {
+            List<Mailbox> mailboxes = DatabaseManager.GetMailboxes(account.AccountName);
+
+            for (int i = mailboxes.Count - 1; i >= 0; --i)
+            {
+                if (mailboxes[i].Flags.Contains(@"\Noselect"))
+                {
+                    mailboxes.RemoveAt(i);
+                }
+            }
+
+            // Sync inbox immediately from a separate thread.
+            foreach (Mailbox mailbox in mailboxes)
+            {
+                if (mailbox.DisplayName.ToLower() == "inbox")
+                {
+                    Task.Run(() => { SyncMessage(account, mailbox.DirectoryPath); });
+                    mailboxes.Remove(mailbox);
+                    break;
+                }
+            }
+
+            // Sync the other mailboxes one at a time.
+            List<string> mailboxNames = new List<string>();
+            foreach (Mailbox mbox in mailboxes)
+            {
+                mailboxNames.Add(mbox.DirectoryPath);
+            }
+            SyncMessagesMultipleMailboxes(account, mailboxNames);
+        }
+
+        // Syncs messages one mailbox at a time.
+        private async void SyncMessagesMultipleMailboxes(Account account, List<string> mailboxNames)
+        {
+            foreach (string mailboxName in mailboxNames)
+            {
+                await Task.Run(() => { SyncMessage(account, mailboxName); });
+            }
+        }
+
+        private void SyncMessage(Account account, string mailboxName)
+        {
+            ImapClient imapClient = new ImapClient(account);
+            if (!imapClient.Connect())
+            {
+                Trace.WriteLine(imapClient.Error);
+                return;
+            }
+
+            ExamineResult status;
+            bool readOnly = true;
+            if (!imapClient.SelectMailbox(mailboxName, readOnly, out status))
+            {
+                Trace.WriteLine(imapClient.Error);
+                imapClient.Disconnect();
+                return;
+            }
+
+            // Download recent messages.
+            int localLargestUid = DatabaseManager.GetMaxUid(account.AccountName, mailboxName);
+            int serverLargestUid = status.UidNext - 1;
+            if (serverLargestUid > localLargestUid)
+            {
+                SyncHelper(account, mailboxName, imapClient, localLargestUid + 1, serverLargestUid);
+            }
+
+            // Download old messages. (Maybe the program shut down previously in the middle of downloading?)
+            int localSmallestUid = DatabaseManager.GetMinUid(account.AccountName, mailboxName);
+            if (localSmallestUid > 1)
+            {
+                SyncHelper(account, mailboxName, imapClient, 1, localSmallestUid - 1);
+            }
+
+            imapClient.Disconnect();
+        }
+
+        private void SyncHelper(Account account, string mailboxName, ImapClient imapClient, int firstUid, int lastUid)
+        {
+            int messagesCount = lastUid - firstUid + 1;
+            int downloadChunk = 30;
+            int startUid = lastUid - downloadChunk + 1;
+            int endUid = lastUid;
+
+            while (endUid >= firstUid)
+            {
+                if (startUid < firstUid)
+                {
+                    startUid = firstUid;
+                }
+                List<Message> msgs = imapClient.FetchHeaders(startUid, endUid - startUid + 1, true);
+                if (msgs.Count > 0)
+                {
+                    foreach (Message msg in msgs)
+                    {
+                        msg.AccountName = account.AccountName;
+                        msg.MailboxPath = mailboxName;
+                        Messages.Add(msg);
+                        OnMessageAdded(msg);
+                    }
+                    DatabaseManager.StoreMessages(msgs);
+                }
+
+                endUid = startUid - 1;
+                startUid = endUid - downloadChunk + 1;
+            }
+        }
+
+    }
+}
