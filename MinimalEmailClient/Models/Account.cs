5dcab5ee9fab0bbd7bf769a69028b4eac4d6890f
YongCho <yongjec@gmail.com>
2016-03-25 18:53:15 -0400

    Initialize string members of Account class to empty string

--- MinimalEmailClient/Models/Account.cs
+++ MinimalEmailClient/Models/Account.cs
@@ -1,37 +1,36 @@
 ﻿using Prism.Mvvm;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace MinimalEmailClient.Models
 {
     public class Account : BindableBase
     {
-        private string accountName;
+        private string accountName = string.Empty;
         public string AccountName
         {
             get { return this.accountName; }
             set { SetProperty(ref this.accountName, value); }
         }
-        private string emailAddress;
+        private string emailAddress = string.Empty;
         public string EmailAddress
         {
             get { return this.emailAddress; }
             set { SetProperty(ref this.emailAddress, value); }
         }
 
-        private string imapServerName;
+        private string imapServerName = string.Empty;
         public string ImapServerName
         {
             get { return this.imapServerName; }
             set { SetProperty(ref this.imapServerName, value); }
         }
-        private string imapLoginName;
+        private string imapLoginName = string.Empty;
         public string ImapLoginName
         {
             get { return this.imapLoginName; }
             set { SetProperty(ref this.imapLoginName, value); }
         }
-        private string imapLoginPassword;
+        private string imapLoginPassword = string.Empty;
         public string ImapLoginPassword
         {
             get { return this.imapLoginPassword; }
@@ -44,19 +43,19 @@ namespace MinimalEmailClient.Models
             set { SetProperty(ref this.imapPortNumber, value); }
         }
 
-        private string smtpServerName;
+        private string smtpServerName = string.Empty;
         public string SmtpServerName
         {
             get { return this.smtpServerName; }
             set { SetProperty(ref this.smtpServerName, value); }
         }
-        private string smtpLoginName;
+        private string smtpLoginName = string.Empty;
         public string SmtpLoginName
         {
             get { return this.smtpLoginName; }
             set { SetProperty(ref this.smtpLoginName, value); }
         }
-        private string smtpLoginPassword;
+        private string smtpLoginPassword = string.Empty;
         public string SmtpLoginPassword
         {
             get { return this.smtpLoginPassword; }
