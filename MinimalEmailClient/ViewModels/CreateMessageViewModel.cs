84d9d6c4116ef93e67be4a46a7951027f09c8397
umin1234 <yoo_cha@student.uml.edu>
2016-03-26 13:41:49 -0400

    minor changes to viewmodel

--- MinimalEmailClient/ViewModels/CreateMessageViewModel.cs
+++ MinimalEmailClient/ViewModels/CreateMessageViewModel.cs
@@ -15,18 +15,29 @@ namespace MinimalEmailClient.ViewModels
         {
             SendCommand = new SendMailCommand(this);
         }
+        
+        private Account _fromAccount = new Account();
+        public Account FromAccount
+        {
+            get
+            {
+                return _fromAccount;
+            }
+        }
+        private WriteNewMessageNotification notification;
+        public ICommand SendCommand { get; }
 
+        // Gets or sets a System.Boolean value indicating whether the Email can be sent.
         public bool CanSend
         {
             get
             {
-                if (CurrentAccount == null)
+                if (FromAccount == null)
                     return false;
                 return true;
             }
         }
 
-        private WriteNewMessageNotification notification;
         public INotification Notification
         {
             get
@@ -46,29 +57,17 @@ namespace MinimalEmailClient.ViewModels
                     else
                     {
                         Trace.WriteLine(this.notification.CurrentAccount);
-                        CurrentAccount = this.notification.CurrentAccount;
+                        _fromAccount = this.notification.CurrentAccount;
                     }
                 }
             }
         }
 
-        public ICommand SendCommand { get; private set; }
-
         public Action FinishInteraction { get; set; }
-
-        private Account currentAccount;
-        public Account CurrentAccount
-        {
-            get { return this.currentAccount; }
-            set
-            {
-                SetProperty(ref this.currentAccount, value);
-            }
-        }
 
         public void SendEmail()
         {
-
+            Trace.WriteLine("Sending from server: " + _fromAccount.SmtpServerName);
         }
     }
 }
--- MinimalEmailClient/Views/CreateMessageView.xaml.cs
+++ MinimalEmailClient/Views/CreateMessageView.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System.Windows.Controls;
-using System.Collections.Generic;
 using MinimalEmailClient.ViewModels;
-using MinimalEmailClient.Models;
 using System.Diagnostics;
 
 namespace MinimalEmailClient.Views
@@ -11,65 +9,10 @@ namespace MinimalEmailClient.Views
     /// </summary>
     public partial class CreateMessageView : UserControl
     {
-        private string smtpServerName = string.Empty;
-        private int smtpPortNumber;
-        private string smtpLoginName = string.Empty;
-        private string smtpLoginPassword = string.Empty;
-        private List<string> receivers;
-        private string subject = string.Empty;
-        private string messageBody = string.Empty;
-        private CreateMessageViewModel viewModel;
-        private Account currentAccount;
-
-        public Account CurrentAccount
-        {
-            get { return currentAccount; }
-        }
-
         public CreateMessageView()
         {
-            viewModel = new CreateMessageViewModel();
-            this.DataContext = viewModel;
-            currentAccount = viewModel.CurrentAccount;
-
             InitializeComponent();
-        }
-
-        private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
-        {
-            AccountManager accountManager = AccountManager.Instance;
-            if (accountManager.Accounts.Count > 0)
-            {
-                //From.Text = currentAccount.EmailAddress;
-                this.smtpServerName = currentAccount.SmtpServerName;
-                this.smtpPortNumber = currentAccount.SmtpPortNumber;
-                this.smtpLoginName = currentAccount.SmtpLoginName;
-                this.smtpLoginPassword = currentAccount.SmtpLoginPassword;
-
-                Trace.WriteLine("Smtp Server Name: " + this.smtpServerName);
-                Trace.WriteLine("Smtp Port Number: " + this.smtpPortNumber);
-                Trace.WriteLine("Smtp Login Name: " + this.smtpLoginName);
-            }
-        }
-
-        private void SendButton_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            this.receivers = new List<string>();
-            this.receivers.Add(To_TextBox.Text);
-            this.receivers.Add(Cc_TextBox.Text);
-            this.subject = Subject_TextBox.Text;
-            this.messageBody = Body_TextBox.Text;
-            Trace.WriteLine("Sending...");
-        }
-
-        private void AttachButton_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-
-        }
-
-        private void To_TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
+            this.DataContext = new CreateMessageViewModel();
         }
     }
 }
