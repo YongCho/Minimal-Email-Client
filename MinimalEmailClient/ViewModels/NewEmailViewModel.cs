0554d255fe5a396d0c89a5898ac9923fadca70ef
umin18 <umin18@gmail.com>
2016-04-20 20:57:24 -0400

    Create function to validated email address before appending recipient header string to reply message. Fixes #45

--- MinimalEmailClient/ViewModels/NewEmailViewModel.cs
+++ MinimalEmailClient/ViewModels/NewEmailViewModel.cs
@@ -16,6 +16,7 @@ using MimeKit;
 using MimeKit.IO;
 using MimeKit.IO.Filters;
 using System.Collections.ObjectModel;
+using System.Globalization;
 
 namespace MinimalEmailClient.ViewModels
 {
@@ -386,6 +387,7 @@ namespace MinimalEmailClient.ViewModels
         #endregion
         private string[] genericDomains = { ".com", ".org", ".net", ".int", ".edu", ".gov", ".mil", };
         public Action FinishInteraction { get; set; }
+        bool invalid = false;
 
         string ExtractSender(string rawSender)
         {
@@ -393,16 +395,62 @@ namespace MinimalEmailClient.ViewModels
             string[] parsedSender = rawSender.Split(delimiterChars);
             foreach (string s in parsedSender)
             {
-                foreach (string gd in genericDomains)
-                {
-                    if (s.EndsWith(gd))
-                    {
-                        return s;
-                    }
-                }
+                if (IsValidEmail(s))
+                    return s;
             }
             Trace.WriteLine("sender email address cannot be found..");
             return null;
         }
+
+        public bool IsValidEmail(string strIn)
+        {
+            invalid = false;
+            if (String.IsNullOrEmpty(strIn))
+                return false;
+
+            // Use IdnMapping class to convert Unicode domain names.
+            try
+            {
+                strIn = Regex.Replace(strIn, @"(@)(.+)$", this.DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+
+            if (invalid)
+                return false;
+
+            // Return true if strIn is in valid e-mail format.
+            try
+            {
+                return Regex.IsMatch(strIn,
+                      @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                      @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                      RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+
+        private string DomainMapper(Match match)
+        {
+            IdnMapping idn = new IdnMapping();
+
+            string domainName = match.Groups[2].Value;
+            try
+            {
+                domainName = idn.GetAscii(domainName);
+            }
+            catch (ArgumentException)
+            {
+                invalid = true;
+            }
+            return match.Groups[1].Value + domainName;
+        }
+
     }
 }
